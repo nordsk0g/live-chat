06ef42298c6b7c87dc8ac3a15465c46411e0d07b
Steve Christian <steven.tr.christian@gmail.com>
2021-11-12 18:51:22 +0100

    added getUser to retrieve user details

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,8 @@
 <template>
-  <nav>
+  <nav v-if="user">
     <div>
-      <p>Hey there... [displayName]</p>
-      <p class="email">Currently logged in as... [email]</p>
+      <p>Hey there, {{ user.displayName }}</p>
+      <p class="email">Currently logged in as {{ user.email }}</p>
     </div>
     <button @click="handleLogout">Logout</button>
   </nav>
@@ -10,19 +10,24 @@
 
 <script>
 import useLogout from '../composables/useLogout'
+import getUser from '../composables/getUser'
+import { useRouter } from 'vue-router'
 
 export default {
   setup() {
     const { error, logout } = useLogout()
+    const { user } = getUser()
+    const router = useRouter()
 
     const handleLogout = async () => {
       await logout()
       if (!error.value) {
+        // router.push({ name: 'Welcome' })
         console.log('user logged out')
       }
     }
 
-    return { handleLogout, error }
+    return { handleLogout, error, user }
   }
 }
 </script>

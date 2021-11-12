de647230bcdea61dd07f028729c8b4b9fa675362
Steve Christian <steven.tr.christian@gmail.com>
2021-11-12 10:03:10 +0100

    first commit of live-chat

---
+++ src/views/Welcome.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="welcome container">
+    <p>Welcome</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .welcome {
+    text-align: center;
+    padding: 1.25em 0;
+  }
+</style>
